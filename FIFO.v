b42b4765a66acd0564b0dca553d8f8055c090ee3
shahpankti2302 <124534100+shahpankti2302@users.noreply.github.com>
2023-05-09 17:39:27 -0700

    Update FIFO.v

--- FIFO.v
+++ FIFO.v
@@ -1,4 +1,9 @@
-module synchronous_fifo #(parameter DEPTH=8, DATA_WIDTH=8) (
+module synchronous_fifo 
+  #(
+  parameter DEPTH=8, 
+  DATA_WIDTH=8
+  ) 
+(
   input clk, rst_n,
   input w_en, r_en,
   input [DATA_WIDTH-1:0] data_in,
@@ -10,29 +15,42 @@ module synchronous_fifo #(parameter DEPTH=8, DATA_WIDTH=8) (
   reg [DATA_WIDTH-1:0] fifo[DEPTH];
   
   // Set Default values on reset.
-  always@(posedge clk) begin
-    if(!rst_n) begin
-      w_ptr <= 0; r_ptr <= 0;
+  always@(posedge clk) 
+  begin
+    if(!rst_n) 
+    begin
+      w_ptr <= 0; 
+      r_ptr <= 0;
       data_out <= 0;
+      for(int i=0 ;i<32; i++)
+            begin
+              fifo[i] <= 0;
+            end
     end
   end
   
   // To write data to FIFO
-  always@(posedge clk) begin
-    if(w_en & !full)begin
+  always@(posedge clk) 
+  begin
+    if(w_en & !full)
+    begin
       fifo[w_ptr] <= data_in;
       w_ptr <= w_ptr + 1;
     end
   end
   
   // To read data from FIFO
-  always@(posedge clk) begin
-    if(r_en & !empty) begin
+  always@(posedge clk) 
+  begin
+    if(r_en & !empty) 
+    begin
       data_out <= fifo[r_ptr];
       r_ptr <= r_ptr + 1;
     end
   end
   
+  //to assign full and empty flags
   assign full = ((w_ptr+1'b1) == r_ptr);
   assign empty = (w_ptr == r_ptr);
+
 endmodule
