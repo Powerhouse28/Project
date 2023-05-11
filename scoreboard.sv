68b60a62ac0e84024339913349eb7c45495da226
shahpankti2302 <124534100+shahpankti2302@users.noreply.github.com>
2023-05-10 22:38:04 -0700

    Update scoreboard.sv

--- scoreboard.sv
+++ scoreboard.sv
@@ -1,6 +1,6 @@
 //`include "transaction.sv"
 class scoreboard;
-mailbox mon2scb scr2dri;
+mailbox mon2scb dri2scr;
  int no_trans;
  bit[7:0]fifo[DEPTH];
  bit [$clog2(DEPTH)-1:0] w_ptr, r_ptr;
@@ -8,7 +8,9 @@ mailbox mon2scb scr2dri;
  
  function new(mailbox mon2scb,scr2dri);
    this.mon2scb = mon2scb;
-  this.scr2dri = scr2dri;
+  mon2scb=new();
+  this.dri2scr = dri2scr;
+  dri2scr=new();
    foreach(fifo[i])begin
     fifo[i] = 8'hff;
    end
@@ -16,15 +18,17 @@ mailbox mon2scb scr2dri;
  
   task main;
    forever begin   
-    transaction trans_score, trans_score_out;
+    transaction trans_score_in, trans_score_out;
     #50
+      dri2scr=new();
+      mon2scb=new();
     mon2scb.get(trans_score_out);
-    scr2dri.get(trans_score_in)
-    if(trans_score.w_en)begin
+    dri2scr.get(trans_score_in);
+    if(trans_score_out.wd_en)begin
      fifo[w_ptr] = trans_score_out.data_in;
       w_ptr++;
     end  
-    if(trans_score.r_en)begin
+    if(trans_score_out.rd_en)begin
      if(trans_score_out.data_out == fifo[r_ptr])begin
         r_ptr++;
         $display("yup");
@@ -33,10 +37,12 @@ mailbox mon2scb scr2dri;
         $display("nop");
       end
     end
-    if(trans_score.full)begin
+    assert (trans_score_out.data_out == trans_score_in.data_out) $display ("%0t      Output is %h and is as expected Success",$time, trans_score_out.data_out);
+
+    if(trans_score_out.full)begin
       $display("fifo is full");
     end
-    if(trans_score.empty)begin
+    if(trans_score_out.empty)begin
       $display("fifo is empty");
     end
     no_trans++;
