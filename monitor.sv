1129eef3b195bf11ce1d17c5914778b307812c5b
Meg0705 <124839521+Meg0705@users.noreply.github.com>
2023-05-11 16:33:14 -0700

    monitor.sv

--- monitor.sv
+++ monitor.sv
@@ -1,14 +1,14 @@
 `define MONITOR_IF fifo_intf.monitor_cb
+
 class monitor;
 virtual fifo_intf vif_fifo;
 mailbox mon2scb; // initialising the mailbox
 
 
-function new(fifo_if fifo_inst,mailbox mon2scb);
-this.fifo_inst =fifo_inst;
+function monitor :: new(virtual fifo_intf vif_fifo,mailbox mon2scb);
+this.vif_fifo =vif_fifo;
 this.mon2scb = mon2scb;
 endfunction
-
 
 task main;
 forever begin
@@ -18,21 +18,14 @@ transaction trans_mon;
   forever begin
    transaction trans_mon;
    trans_mon = new();
-   @(fifo_inst.monitor_cb);
+   
+   wait(vif_fifo.rd_en);
+   $display("Reading");
+   @(vif_fifo.monitor_cb);
 
-   if(MONITOR_IF.wr_en||MONITOR_IF.rd_en); // wait was used 
-   begin
-   	if(MONITOR_IF.wr_en)begin
-    	trans_mon.wr_en = MONITOR_IF.wr_en ;
-    	trans_mon.data_in = MONITOR_IF.data_in; 
-    	trans_mon.full = MONITOR_IF.full;
-    	trans_mon.empty = MONITOR_IF.empty;
-    	$display("\t ADDR= %0h \t DATA IN = %0h",trans_mon.wr_en,trans_mon.data_in);
-   	end
-   @(fifo_inst.monitor_cb);
    if(MONITOR_IF.rd_en)begin
     trans_mon.rd_en = MONITOR_IF.rd_en ;
-    @(fifo_inst.monitor_cb);
+    @(vif_fifo.monitor_cb);
      trans_mon.data_out = MONITOR_IF.data_out;
      trans_mon.full = MONITOR_IF.full;
      trans_mon.empty = MONITOR_IF.empty;
