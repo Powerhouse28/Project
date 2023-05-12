738049ee60ed8f92a5af9d0c465a41078589b255
shahpankti2302 <124534100+shahpankti2302@users.noreply.github.com>
2023-05-11 17:04:57 -0700

    Update interface.sv

--- interface.sv
+++ interface.sv
@@ -5,30 +5,30 @@
 
 interface fifo_if;
 reg clk,rst_n;
-reg w_en, r_en;
+reg wr_en, rd_en;
 reg [DATA_WIDTH-1:0] data_in;
 wire [DATA_WIDTH-1:0] data_out;
 wire full, empty;
 
 clocking driver_cb@(posedge clk);
     default input #1 output #1;
-    input clk,rst;
+    input clk,rst_n;
     output data_in;
     output wr_en,rd_en;
-    input data_op;
+    input data_out;
     input full,empty;
 endclocking
 
 clocking monitor_cb@(posedge clk);
     default input #1 output #1;
-    input clk,rst;
+    input clk,rst_n;
     input data_in;
     input wr_en,rd_en;
-    input data_op;
+    input data_out;
     input full,empty;
 endclocking
   
-modport driver(clocking driver_cb,input clk,rst);
-modport monitor(clocking monitor_cb,input clk,rst);
+    modport driver(clocking driver_cb,input clk,rst_n);
+        modport monitor(clocking monitor_cb,input clk,rst_n);
 
 endinterface
