353c94ca3074349ddb7c82830853f7658afa73a2
shahpankti2302 <124534100+shahpankti2302@users.noreply.github.com>
2023-05-14 18:39:27 -0700

    Update transaction.sv

--- transaction.sv
+++ transaction.sv
@@ -1,6 +1,6 @@
 class transaction;
   //only input signals for randomisation and no ports
-  bit rst_n,clk;
+  bit clk;
   rand bit [7:0] data_in;
   rand bit wr_en,rd_en,rst_n;	
   bit[7:0]data_out;
