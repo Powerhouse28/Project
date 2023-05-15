e69c143c95837afe37c6720b8ff005429dae55e8
shahpankti2302 <124534100+shahpankti2302@users.noreply.github.com>
2023-05-14 18:48:12 -0700

    Update driver.sv

--- driver.sv
+++ driver.sv
@@ -26,8 +26,9 @@ class driver;
   task reset;
   tr = new();
  vif_fifo.rst_n=1; 
-  $display("resetting");
+  
     if(vif_fifo.rst_n) begin
+      $display("resetting");
     `DRIVER_IF.data_in <= 0;
     `DRIVER_IF.wr_en <= 0;
    `DRIVER_IF.rd_en <= 0;
