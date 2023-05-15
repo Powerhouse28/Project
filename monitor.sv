c3f773663adb8fd0c6f2bc07b283cebf7e8dc727
Powerhouse28 <26279434+Powerhouse28@users.noreply.github.com>
2023-05-15 10:39:39 -0700

    Update monitor.sv

--- monitor.sv
+++ monitor.sv
@@ -18,12 +18,12 @@ endfunction
    trans_mon = new();
    
    //wait(vif_fifo.rd_en);
-   if(vif_fifo.rd_en);
+ //  if(vif_fifo.rd_en);
    
-   $display("Reading");
+ //  $display("Reading");
  //  @(vif_fifo.monitor_cb);
 
-   if(`MONITOR_IF.rd_en)begin
+ //  if(`MONITOR_IF.rd_en)begin
     trans_mon.rd_en = `MONITOR_IF.rd_en ;
  //   @(vif_fifo.monitor_cb);
      trans_mon.data_out = `MONITOR_IF.data_out;
