bca7872502788dac36569d9d816d0b9fe25f9b90
Powerhouse28 <26279434+Powerhouse28@users.noreply.github.com>
2023-05-17 00:54:44 -0700

    added

--- scoreboard.sv
+++ scoreboard.sv
@@ -29,7 +29,7 @@ mailbox #(transaction) mon2scb, drv2scr;
   task main;
    //forever
    $display("\t\t Scoreboard ");
-   w_ptr=0;
+   w_ptr=1;
    r_ptr=0;
    repeat (10) begin   
 	 //  $display("yup2");
